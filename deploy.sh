dd56d14da1ed547a258c2148c27e017d5a37c6b1
王千一 <wangqianyi03@meituan.com>
2022-01-30 03:53:48 +0800

    modify deploy action

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # 确保脚本抛出遇到的错误
 set -e
 echo $1
+REPO="https://xdushepherd91:$1@github.com/vue-blog.git"
+
+echo $REPO
 # 生成静态文件
 yarn install
 yarn build
@@ -24,7 +27,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
- git push -f https://xdushepherd91:$1@github.com/vue-blog.git master:gh-pages
+ git push -f $REPO master:gh-pages
 
 # git push -f git@github.com:xdushepherd91/vue-blog.git master:gh-pages
 
